82fd6f89d747a076c6d75e975edc4a26d51371dd
Debut_12 <iniziato@naver.com>
2023-06-16 13:02:28 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.13 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = Arrays.copyOf(arr, arr.length);
+
+		for(int[] query : queries) {
+			int temp = 0;
+
+			temp = answer[query[0]];
+			answer[query[0]] = answer[query[1]];
+			answer[query[1]] = temp;
+		}
+        
+        return answer;
+    }
+}
